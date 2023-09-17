28efa031aed146ba527987a18f54730dc4ff7b4e
Agnarz <45544559+Agnarz@users.noreply.github.com>
2023-09-17 12:59:58 -0400

    formatting

--- web/src/AdminMenu/index.tsx
+++ web/src/AdminMenu/index.tsx
@@ -1,60 +1,60 @@
-import React, { useState } from "react";
-import { createStyles, Tabs } from "@mantine/core";
-import { useNuiEvent } from "../hooks/useNuiEvent";
+import React, { useState } from 'react';
+import { createStyles, Tabs } from '@mantine/core';
+import { useNuiEvent } from '../hooks/useNuiEvent';
 
 const useStyles = createStyles((theme) => ({
   root: {
-    position: "absolute",
-    transform: "translate(-0%, -50%)",
-    top: "50%",
-    right: "0.5%",
+    position: 'absolute',
+    transform: 'translate(-0%, -50%)',
+    top: '50%',
+    right: '0.5%',
     width: 440,
-    height: "75vh",
-    display: "flex",
-    flexDirection: "column",
-    fontFamily: "Poppins, sans-serif",
-    transition: "all 0.25s ease",
+    height: '75vh',
+    display: 'flex',
+    flexDirection: 'column',
+    fontFamily: 'Poppins, sans-serif',
+    transition: 'all 0.25s ease'
   },
   container: {
-    display: "flex",
-    flexDirection: "column",
-    position: "relative",
-    height: "100%",
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'relative',
+    height: '100%'
   },
   tabs: {
-    position: "relative",
-    height: "100%!important",
+    position: 'relative',
+    height: '100%!important'
   },
   sidebar: {
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "top",
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'top',
     width: 44,
     padding: 4,
     gap: 4
   },
   panel: {
     background: theme.colors.ag[7],
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
     borderRadius: 0,
     padding: 8,
-    overflow: "hidden",
+    overflow: 'hidden'
   },
   tabBtn: {
-    background: theme.colors.ag[8] + "f2",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    background: theme.colors.ag[8] + 'f2',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     width: 44,
-    ":hover": {
-      background: theme.colors.ag[2],
-    },
-  },
+    ':hover': {
+      background: theme.colors.ag[2]
+    }
+  }
 }));
 
 const AdminMenu: React.FC = () => {
@@ -62,7 +62,7 @@ const AdminMenu: React.FC = () => {
   const [visible, setVisible] = useState(0);
 
   // Using opacity instead of display: none; because we want to keep the menu mounted
-  useNuiEvent("toggleMenu", (data) => {
+  useNuiEvent('toggleMenu', (data) => {
     if (data) {
       setVisible(0.95);
     } else {
@@ -71,20 +71,20 @@ const AdminMenu: React.FC = () => {
   });
 
   const tabs = [
-    { name: "Commands", icon: "fas fa-hat-wizard" },
-    { name: "Players", icon: "fas fa-users" }
+    { name: 'Commands', icon: 'fas fa-hat-wizard' },
+    { name: 'Players', icon: 'fas fa-users' }
   ];
 
   return (
     <div className={classes.root} style={{ opacity: visible }}>
       <div className={classes.container}>
-        <Tabs variant="pills" keepMounted={true} defaultValue="Commands" orientation="vertical" className={classes.tabs}>
+        <Tabs variant='pills' keepMounted={true} defaultValue='Commands' orientation='vertical' className={classes.tabs}>
           <Tabs.List className={classes.sidebar}>
             {tabs.map((v, index) => (
               <React.Fragment key={index}>
                 <Tabs.Tab
                   icon={
-                    <i style={{ color: "white", fontSize: 18 }} className={v.icon}  />
+                    <i className={v.icon} style={{ color: 'white', fontSize: 18 }} />
                   }
                   value={v.name}
                   className={classes.tabBtn}
@@ -93,11 +93,11 @@ const AdminMenu: React.FC = () => {
             ))}
           </Tabs.List>
 
-          <Tabs.Panel value="Commands" className={classes.panel}>
+          <Tabs.Panel value='Commands' className={classes.panel}>
 
           </Tabs.Panel>
 
-          <Tabs.Panel value="Players" className={classes.panel}>
+          <Tabs.Panel value='Players' className={classes.panel}>
 
           </Tabs.Panel>
         </Tabs>
