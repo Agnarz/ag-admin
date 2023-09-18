d152b3361c29d81f64360306824447e5945bfe0a
Agnarz <45544559+Agnarz@users.noreply.github.com>
2023-09-18 13:35:43 -0400

    tragets/players callbacks

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,28 @@
+lib.callback.register('ag:getTargets', function()
+    local targets = GetPlayers()
+
+    for i, v in ipairs(targets) do
+        local target = {
+            label = "(" .. v .. ") " .. GetPlayerName(v),
+            value = v
+        }
+        targets[i] = target
+    end
+
+    return targets
+end)
+
+lib.callback.register('ag:getPlayers', function()
+    local players = GetPlayers()
+
+    for i, v in ipairs(players) do
+        players[i] = {
+            source = v,
+            label = "(" .. v .. ") " .. GetPlayerName(v),
+            license = GetPlayerIdentifierByType(v, 'license'),
+        }
+    end
+
+    return players
+end)
 
