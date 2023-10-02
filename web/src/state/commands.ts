085a5f1a707cb32d95281dafe3f7f5964e37c597
Agnarz <45544559+Agnarz@users.noreply.github.com>
2023-10-02 19:34:16 -0400

    Update state/commands.ts
    add favorites atom

--- web/src/state/commands.ts
+++ web/src/state/commands.ts
@@ -6,3 +6,8 @@ const commandsAtom = atom<CommandProps[]>([]);
 export const useCommands = () => useAtom(commandsAtom);
 export const useCommandsValue = () => useAtomValue(commandsAtom);
 export const useSetCommands = () => useSetAtom(commandsAtom);
+
+const favorites = atom<string[]>([]);
+export const useFavorites = () => useAtom(favorites);
+export const useFavoritesValue = () => useAtomValue(favorites);
+export const useSetFavorites = () => useSetAtom(favorites);
